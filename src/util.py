0be0699288d492be65c34e114554488e1fe4e817
smitsudani555 <smitsudani555@gmail.com>
2025-01-21 19:12:36 +0530

    Model Trainer Done

--- src/util.py
+++ src/util.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -17,3 +18,53 @@ def save_object(file_path, obj):
             
     except Exception as e:
         raise CustomException(e, sys)
+    
+# def evaluate_model(x_train,y_train,x_test,y_test,models):
+#     try:
+#         report={}
+
+#         for i in range(len(list(models))):
+#             model=list(models.values())[i]
+
+#             model.fit(x_train,y_train)
+
+#             y_train_pred=model.predict(x_train)
+#             y_test_pred=model.predict(x_test)
+
+#             train_model_score=r2_score(y_train,y_train_pred)
+#             test_model_score=r2_score(y_test,y_test_pred)
+
+#             report[list(model.keys())[i]]=test_model_score
+            
+#         return report
+def evaluate_model(x_train, y_train, x_test, y_test, models):
+    """
+    Evaluates multiple models and returns their test r2 scores.
+    """
+    try:
+        report = {}
+
+        # Loop through the models dictionary
+        for name, model in models.items():  # Use .items() to get both the key (name) and the value (model object)
+            # Train the model
+            model.fit(x_train, y_train)
+
+            # Predict on training and testing data
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+
+            # Calculate r2 scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            # Save the test model score to the report dictionary
+            report[name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+    except Exception as e:
+        raise CustomException(e,sys)
